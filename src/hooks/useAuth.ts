8f2d994f81f58be32c8b128f63fbb6082e66f53a
Fata Nabil Fikri <fatanabil88@gmail.com>
2025-03-17 13:44:00 +0700

    fix: fix auth hook and store

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,41 +1,46 @@
-import { useEffect, useState } from 'react';
-import { useAuthStore } from '../store/authStore';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { API_BASE_URL } from '../utils/constant';
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
+import { api } from "../utils/api";
 
 const useAuth = () => {
-    const { accessToken, setAccessToken, setUser, logout } = useAuthStore();
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
-    const location = useLocation();
+  const { accessToken, setAccessToken, setUser, logout } = useAuthStore();
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    useEffect(() => {
-        const checkAuth = async () => {
-            try {
-                if (!accessToken) {
-                    const res = await fetch(`${API_BASE_URL}/refresh-token`, { method: 'POST', credentials: 'include' });
-                    const data = await res.json();
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        if (!accessToken) {
+          const res = await api.post("/refresh-token");
+          const { data } = await res.data;
 
-                    if (!res.ok) throw new Error(data.message);
+          setAccessToken(data.accessToken);
+          setUser(data.user);
+        }
+      } catch (error) {
+        console.error(`Auth failed : ${error}`);
+        logout();
+        if (error && location.pathname !== "/login") {
+          navigate("/login", { replace: true });
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
 
-                    setAccessToken(data.accessToken);
-                    setUser(data.user);
-                }
-            } catch (error) {
-                console.error(`Auth failed : ${error}`);
-                logout();
-                if (error && location.pathname !== '/login') {
-                    navigate('/login', { replace: true });
-                }
-            } finally {
-                setLoading(false);
-            }
-        };
+    checkAuth();
+  }, [
+    accessToken,
+    setAccessToken,
+    location.pathname,
+    logout,
+    navigate,
+    setUser,
+  ]);
 
-        checkAuth();
-    }, [accessToken, setAccessToken, location.pathname, logout, navigate, setUser]);
-
-    return { loading };
+  return { loading };
 };
 
 export default useAuth;
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,21 +1,23 @@
-import { create } from 'zustand';
+import { create } from "zustand";
+import { User } from "../schemas/User";
 
 interface AuthState {
-    accessToken: string | null;
-    user: unknown;
-    setAccessToken: (token: string) => void;
-    setUser: (user: unknown) => void;
-    loading: boolean;
-    setLoading: (loading: boolean) => void;
-    logout: () => void;
+  accessToken: string | null;
+  user: User;
+  setAccessToken: (token: string) => void;
+  setUser: (user: User) => void;
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
+  logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-    accessToken: null,
-    user: null,
-    setAccessToken: (token) => set({ accessToken: token }),
-    setUser: (user) => set({ user }),
-    loading: false,
-    setLoading: (loading) => set({ loading }),
-    logout: () => set({ accessToken: null, user: null }),
+  accessToken: null,
+  user: { id: "", email: "", name: "" },
+  setAccessToken: (token) => set({ accessToken: token }),
+  setUser: (user) => set({ user }),
+  loading: false,
+  setLoading: (loading) => set({ loading }),
+  logout: () =>
+    set({ accessToken: null, user: { id: "", email: "", name: "" } }),
 }));

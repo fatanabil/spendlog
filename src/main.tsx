45f2e129a565dde483cf1884bfa0ae423ac4cacf
Fata Nabil Fikri <fatanabil88@gmail.com>
2025-03-22 00:33:29 +0700

    feat: use tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./components/ThemeProvider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="/spendlog">
       <ThemeProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,

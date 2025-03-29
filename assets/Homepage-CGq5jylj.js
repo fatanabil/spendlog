15d773bb312162e60835bc2a9d66d8b72655f980
Fata Nabil Fikri <fatanabil88@gmail.com>
2025-03-29 12:29:58 +0700

    Updates

--- assets/Homepage-CGq5jylj.js
+++
@@ -1,1 +0,0 @@
-import{u as t,a as r,b as o,j as s,L as n}from"./index-BHebYxy1.js";const u=()=>{t();const{accessToken:a}=r(),e=o();return a&&e("/dashboard",{replace:!0}),s.jsxs("main",{children:[s.jsx("h1",{children:"THIS IS HOMEPAGE"}),s.jsx(n,{to:"/dashboard",children:"GO TO DASHBOARD"})]})};export{u as default};

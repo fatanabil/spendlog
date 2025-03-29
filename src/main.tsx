74c7949cdbdd2b7bdce9b935af0b7ca2c4ab6d00
Fata Nabil Fikri <fatanabil88@gmail.com>
2025-03-29 12:31:47 +0700

    fix: handle 404 with index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./components/ThemeProvider.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/spendlog">
+    <HashRouter>
       <ThemeProvider>
         <QueryClientProvider client={queryClient}>
           <App />
         </QueryClientProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
